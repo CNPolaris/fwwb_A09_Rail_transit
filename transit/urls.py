0bde227a1fe08f11e93532e6eaa532bdfb7b7a0a
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2021-01-31 19:55:37 +0800

    fix(整理项目): 项目重构

---
+++ transit/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/16 10:48
+# @FileName: urls.py
+# @Author  : CNPolaris
+
+# 引入path
+from django.conf.urls import url, include
+# 正在部署的应用的名称
+from transit import views
+from .apis.urls import echarts_api_urls, manager_urls
+from .imports import load_dataoftrip, load_dataofstation, load_dataofuser, load_dataofworkday
+
+app_name = 'transit'
+
+urlpatterns = [
+    # 首页路由
+    # API模块
+    url(r'^api/', include(echarts_api_urls)),
+    url(r'^api/', include(manager_urls)),
+
+    # import数据模块
+    url('load_dataoftrip', load_dataoftrip, name='load_data'),
+    url('load_dataofworkday', load_dataofworkday, name='load_dataofworkday'),
+    url('load_dataofuser', load_dataofuser, name='load_dataofuser'),
+    url('load_dataofstation', load_dataofstation, name='load_dataofstation')
+]
